ca81d837356e78de1099282e3d06ab68b0865bcb
Mihail Prokin <mihaproka@gmail.com>
2024-07-19 16:43:16 +1000

    Домашние задание № 2

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -7,6 +7,26 @@ const (
 	USDoRUB = 90.0
 )
 
+func readInput() {
+	var amount float64
+	var sourceCurrency string
+	var targetCurrency string
+
+	fmt.Print("Введите сумму: ")
+	fmt.Scan(&amount)
+
+	fmt.Print("Введите исходную валюту (USD, EUR, RUB): ")
+	fmt.Scan(&sourceCurrency)
+
+	fmt.Print("Введите целевую валюту (USD, EUR, RUB): ")
+	fmt.Scan(&targetCurrency)
+
+	convertCurrency(amount, sourceCurrency, targetCurrency)
+}
+
+func convertCurrency(amount float64, sourceCurrency, targetCurrency string) {
+}
+
 func main() {
 	EURoRUB := USDoRUB / USDoEUR
 
